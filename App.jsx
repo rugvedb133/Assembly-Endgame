67c68b5e92cfb2f7f74845d3b75439ca0d61685d
rugvedb133 <rugvedbhise@gmail.com>
2026-05-10 13:07:40 +0530

    Languages List https://scrimba.com/learn-react-c0e/~075e

--- App.jsx
+++ App.jsx
@@ -1,25 +1,48 @@
 import React from "react"
+import { languages } from "./languages"
 
 /**
  * Goal: Build out the main parts of our app
  * 
- * Challenge: Build a status section below the header.
- * For now, you can just hard-code in the styles for
- * a winning game, and we'll make it more dynamic
- * later.
+ * Challenge: Create the language chips. Use the
+ * `languages.js` file to pull in the array of
+ * languages to use, which contains the language
+ * name, background color, and text color.
+ * 
+ * Hint for layout: use a flex container that can wrap
+ * to layout the languages.
  */
 
 export default function AssemblyEndgame() {
+
+    const languageElements = languages.map(lang => {
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+            <span 
+                className="chip" 
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
     return (
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the 
+                <p>Guess the word within 8 attempts to keep the
                 programming world safe from Assembly!</p>
             </header>
             <section className="game-status">
                 <h2>You win!</h2>
                 <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
             </section>
         </main>
     )
