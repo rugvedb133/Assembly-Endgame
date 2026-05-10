8565a6b83b042c26b2f0d350c9d2352a1afe48cf
rugvedb133 <rugvedbhise@gmail.com>
2026-05-10 23:10:03 +0530

    Make the game more a11y-friendly part 1 https://scrimba.com/learn-react-c0e/~0zm3

--- App.jsx
+++ App.jsx
@@ -80,6 +80,8 @@ export default function AssemblyEndgame() {
                 className={className}
                 key={letter}
                 disabled={isGameOver}
+                aria-disabled={guessedLetters.includes(letter)}
+                aria-label={`Letter ${letter}`}
                 onClick={() => addGuessedLetter(letter)}
             >
                 {letter.toUpperCase()}
@@ -130,7 +132,11 @@ export default function AssemblyEndgame() {
                 programming world safe from Assembly!</p>
             </header>
 
-            <section className={gameStatusClass}>
+            <section 
+                aria-live="polite" 
+                role="status" 
+                className={gameStatusClass}
+            >
                 {renderGameStatus()}
             </section>
 
@@ -140,6 +146,17 @@ export default function AssemblyEndgame() {
 
             <section className="word">
                 {letterElements}
+            </section>
+            
+            <section 
+                className="sr-only" 
+                aria-live="polite" 
+                role="status"
+            >
+                <p>Current word: {currentWord.split("").map(letter => 
+                guessedLetters.includes(letter) ? letter + "." : "blank.")
+                .join(" ")}</p>
+            
             </section>
 
             <section className="keyboard">
