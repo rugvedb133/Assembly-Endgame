a4c63a1179a8097734950a707eb246f2ab753a49
rugvedb133 <rugvedbhise@gmail.com>
2026-05-10 14:38:52 +0530

    New Game Button https://scrimba.com/learn-react-c0e/~0ygq

--- App.jsx
+++ App.jsx
@@ -58,6 +58,7 @@ export default function AssemblyEndgame() {
             <section className="keyboard">
                 {keyboardElements}
             </section>
+            <button className="new-game">New Game</button>
         </main>
     )
 }
