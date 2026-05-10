4b8f4d62bf2a3b229d6a2f179a644c3cd40a248d
rugvedb133 <rugvedbhise@gmail.com>
2026-05-10 12:18:05 +0530

    Header Section https://scrimba.com/learn-react-c0e/~0xts

--- App.jsx
+++ App.jsx
@@ -1,28 +1,20 @@
 import React from "react"
 
 /**
- * Project planning:
+ * Goal: Build out the main parts of our app
  * 
- * Questions to ask yourself before writing any code:
- * 
- * - What are the main containers of elements I need
- *   in this app?
- * 
- * 
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * 
- * 
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * 
- * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
  */
 
-export default function Hangman() {
+export default function AssemblyEndgame() {
     return (
         <main>
-            Game goes here
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+            </header>
         </main>
     )
 }
