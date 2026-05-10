644b444d6146f8b5db821cd5e1eb9e70d1a269f0
rugvedb133 <rugvedbhise@gmail.com>
2026-05-10 14:37:27 +0530

    Keyboard https://scrimba.com/learn-react-c0e/~0ygq

--- App.jsx
+++ App.jsx
@@ -5,17 +5,14 @@ import { languages } from "./languages"
  * Goal: Build out the main parts of our app
  * 
  * Challenge: 
- * 1. Save a "currentWord" in state. Initialize as "react".
- * 2. Map over the letters of the word (you'll need to turn 
- *    the string into an array of letters first) and display
- *    each one as a <span>. Capitalize the letters when
- *    displaying them.
- * 3. Style to look like the design. You can get the underline 
- *    effect on the box using `border-bottom`.
+ * Display the keyboard ⌨️. Use <button>s for each letter
+ * since it'll need to be clickable and tab-accessible.
  */
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react")
+    
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     const languageElements = languages.map(lang => {
         const styles = {
@@ -36,6 +33,10 @@ export default function AssemblyEndgame() {
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
     ))
+    
+    const keyboardElements = alphabet.split("").map(letter => (
+        <button key={letter}>{letter.toUpperCase()}</button>
+    ))
 
     return (
         <main>
@@ -53,6 +54,9 @@ export default function AssemblyEndgame() {
             </section>
             <section className="word">
                 {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
             </section>
         </main>
     )
